4fb09e63bc1a212e0500b8af087d2c7eb0d4777b
Lloydee Singgo <itmm103reginnlloydsinggo@gmail.com>
2018-06-28 22:11:00 +0800

    Got a new update

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<title>itmm103activity3</title>
+<body>
+<p>
+	Activity Name: More on PHP Basics <br>
+	Name: Reginn Lloyd H. Singgo <br>
+	Course Title: Advanced Web Developing, Second Year 
+	<br>
+	<a href="Section A/index.html">Link Section A</a> 
+	<br>
+	<a href="Section B/index.html">Link Section B</a>
+	<br>
+	<a href="Section C/index.html">Link Section C</a>
+	<br>
+	<a href="Section D/index.html">Link Section D</a>
+	<br>
+	<a href="Section E/index.html">Link Section E</a>
+	<br>
+	<a href="Section F/index.html">Link Section F</a>
+	<br>
+	<a href="Section G/index.html">Link Section G</a>
+	<br>
+	<a href="Section H/index.html">Link Section H</a>
+	<br>
+	<a href="Section I/index.html">Link Section I</a>
+	<br>
+	
+
+</p>
+</body>
+</html>
